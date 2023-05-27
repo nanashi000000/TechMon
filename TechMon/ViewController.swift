98fcc2fab7c551bf91f7a85a82ab3e60d6f944e4
hiro.kajikawa <hiroshi117000@gmail.com>
2023-05-28 00:23:34 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by hiroshi kajikawa on 2023/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
